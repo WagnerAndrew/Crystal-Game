dcc0b88ccf53ba4458534f617b8a9b65c93dd71c
WagnerAndy <wagner.andrew.james@gmail.com>
2018-11-14 18:03:54 -0700

    close to final still no reset

--- assets/javascript/game.js
+++ assets/javascript/game.js
@@ -1,69 +1,64 @@
 
-$(document).ready(function() { 
-//VARIABLES
+$(document).ready(function () {
 
-var winCount = 0;
-var lossCount = 0;
-var guessSum = 0;
-var numberToMatch = numberToMatch ();
+    //VARIABLES
 
-//FUNCTIONS
+    var winCount = 0;
+    var lossCount = 0;
+    var guessSum = 0;
+    var numberToMatch = randomNumberToMatch();
 
-function numberToMatch () {
-    var randomNumber = Math.floor(Math.random() * (120 - 19) ) + 19;
-    return randomNumber;
-};
+    //FUNCTIONS
 
-function crystalValues (){
-    for (var i = 0; i < 4; i++) {
-
-        var imageCrystal = $("<img>");
-            
-        imageCrystal.addClass(" image-fluid m-3 crystal-image");
-        imageCrystal.attr("src", "http://cdn.playbuzz.com/cdn/35910209-2844-45c0-b099-f4d82878d54f/00261fda-4062-4096-81fd-8cf96b9034e8.jpg");
-        imageCrystal.attr("data-crystalvalue", Math.floor(Math.random() * (12 - 1) ) + 1);
-        $("#crystals").append(imageCrystal);
+    function randomNumberToMatch() {
+        var randomNumber = Math.floor(Math.random() * (120 - 19)) + 19;
+        return randomNumber;
     };
-};
+
+    function crystalValues() {
+        for (var i = 0; i < 4; i++) {
+
+            var imageCrystal = $("<img>");
+
+            imageCrystal.addClass(" image-fluid m-3 crystal-image");
+            imageCrystal.attr("src", "http://cdn.playbuzz.com/cdn/35910209-2844-45c0-b099-f4d82878d54f/00261fda-4062-4096-81fd-8cf96b9034e8.jpg");
+            imageCrystal.attr("data-crystalvalue", Math.floor(Math.random() * (12 - 1)) + 1);
+            $("#crystals").append(imageCrystal);
+        };
+    };
 
 
 
-//PROCESS    
+    //PROCESS    
 
     $("#numberToMatch").text(numberToMatch);
 
-    crystalValues ();
+    crystalValues();
 
-  
-        $(".crystal-image").on("click", function() {
-    
-            var crystalValue = ($(this).attr("data-crystalvalue"));
-            crystalValue = parseInt(crystalValue);
-            console.log ("The value of this crystal is: " + crystalValue);
-            guessSum = guessSum + crystalValue;
 
-        
+    $(".crystal-image").on("click", function () {
 
-            if (guessSum === numberToMatch){
-                $("#guessSum").text("You got " + guessSum + "!" + " You Win!");
-                winCount++;
-                $("#winCount").text(winCount);
-                numberToMatch ();
-                crystalValues ();
-                guessSum = 0;
-        
-            } else if (guessSum < numberToMatch){
-                $("#guessSum").text(guessSum);
+        var crystalValue = ($(this).attr("data-crystalvalue"));
+        crystalValue = parseInt(crystalValue);
+        console.log("The value of this crystal is: " + crystalValue);
+        guessSum = guessSum + crystalValue;
 
-            } else {
-                $("#guessSum").text("You went over with " + guessSum + "!" + " Sorry, you lose!");
-                lossCount++;
-                $("#lossCount").text(lossCount);
-                numberToMatch ();
-                crystalValues ();
-                guessSum = 0;
-            }
-        });            
+
+
+        if (guessSum === numberToMatch) {
+            $("#guessSum").text("You got " + guessSum + "!" + " You Win!");
+            winCount++;
+            $("#winCount").text(winCount);
+
+        } else if (guessSum < numberToMatch) {
+            $("#guessSum").text(guessSum);
+
+        } else {
+            $("#guessSum").text("You went over with " + guessSum + "!" + " Sorry, you lose!");
+            lossCount++;
+            $("#lossCount").text(lossCount);
+        }
+    });
 
 
 })
